0c31884501226a3d451d9d98a09833456a3eb34b
gerardor <gerardor.com@gmail.com>
2017-11-18 21:46:14 -0300

    routing implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
-import BookList from './BookList'
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
+import BookList from './BookList';
+import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+
 
 
 
 class BooksApp extends React.Component {
 
 	state = {
-	/**
-	 * TODO: Instead of using this state variable to keep track of which page
-	 * we're on, use the URL in the browser's address bar. This will ensure that
-	 * users can use the browser's back and forward buttons to navigate between
-	 * pages, as well as provide a good URL they can bookmark and share.
-	 */
-		showSearchPage: false,
 		query: '',
 		books: []
-		
 	}
 
 	componentDidMount() {
@@ -30,7 +25,6 @@ class BooksApp extends React.Component {
 	}
 
 	onChangeShelf(updatedBook, shelf){
-
 		const updatedBooks = this.state.books.map(function(book){
 			if (book.id === updatedBook.id){
 				book.shelf = shelf;
@@ -56,98 +50,113 @@ class BooksApp extends React.Component {
 
 		return (
 			<div className="app">
-				
-				{this.state.showSearchPage ? (
-					
-					<div className="search-books">
-						<div className="search-books-bar">
-						<a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-						<div className="search-books-input-wrapper">
-						
-							<input
-								type='text'
-								placeholder='Search by title or author'
-								value={this.state.query}
-								onChange={(event) => this.updateQuery(event.target.value)}
-							/>
-
-						</div>
-					</div>
-				
-					<div className="search-books-results">
-						<ol className="books-grid"></ol>
-
-						<BookList
-							books={this.state.books}
-							shelf='searching'
-							changeShelf={this.onChangeShelf.bind(this)}
-							query={this.state.query}
-						/>
-					</div>
-				</div>
-				
-				) : (
-				
-				<div className="list-books">
-					<div className="list-books-title">
-						<h1>MyReads</h1>
-					</div>
-
-					<div className="list-books-content">
-						<div>
-							<div className="bookshelf">
-								<h2 className="bookshelf-title">Currently Reading</h2>
-								<div className="bookshelf-books">
+				<Route
+					path='/search'
+					render={
+						() => (
+							<div className="search-books">
+								<div className="search-books-bar">
 									
-									 <BookList
-										books={this.state.books}
-										shelf='currentlyReading'
-										changeShelf={this.onChangeShelf.bind(this)}
-									/>
-								</div>
-							</div>
-						</div>
-					</div>
+									<Link to='/' className="close-search">
+										Close
+									</Link>
+									
+									<div className="search-books-input-wrapper">
+									
+										<input
+											type='text'
+											placeholder='Search by title or author'
+											value={this.state.query}
+											onChange={(event) => this.updateQuery(event.target.value)}
+										/>
 
-					<div className="list-books-content">
-						<div>
-							<div className="bookshelf">
-								<h2 className="bookshelf-title">Want to read</h2>
-								<div className="bookshelf-books">
+									</div>
+								</div>
+						
+								<div className="search-books-results">
+									<ol className="books-grid"></ol>
 
 									<BookList
 										books={this.state.books}
-										shelf='wantToRead'
+										shelf='searching'
 										changeShelf={this.onChangeShelf.bind(this)}
+										query={this.state.query}
 									/>
-
 								</div>
 							</div>
-						</div>
-					</div>
+						)
+					}
+				/>
+				
+				<Route
+					exact path='/'
+					render={
+						() => (
+							<div className="list-books">
+								<div className="list-books-title">
+									<h1>MyReads</h1>
+								</div>
 
-					<div className="list-books-content">
-						<div>
-							<div className="bookshelf">
-								<h2 className="bookshelf-title">Read</h2>
-								<div className="bookshelf-books">
-							
-									<BookList
-										books={this.state.books}
-										shelf='read'
-										changeShelf={this.onChangeShelf.bind(this)}
-									/>
+								<div className="list-books-content">
+									<div>
+										<div className="bookshelf">
+											<h2 className="bookshelf-title">Currently Reading</h2>
+											<div className="bookshelf-books">
+												
+												 <BookList
+													books={this.state.books}
+													shelf='currentlyReading'
+													changeShelf={this.onChangeShelf.bind(this)}
+												/>
+											</div>
+										</div>
+									</div>
+								</div>
 
+								<div className="list-books-content">
+									<div>
+										<div className="bookshelf">
+											<h2 className="bookshelf-title">Want to read</h2>
+											<div className="bookshelf-books">
+
+												<BookList
+													books={this.state.books}
+													shelf='wantToRead'
+													changeShelf={this.onChangeShelf.bind(this)}
+												/>
+
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div className="list-books-content">
+									<div>
+										<div className="bookshelf">
+											<h2 className="bookshelf-title">Read</h2>
+											<div className="bookshelf-books">
+										
+												<BookList
+													books={this.state.books}
+													shelf='read'
+													changeShelf={this.onChangeShelf.bind(this)}
+												/>
+
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<div className="open-search">
+									<Link to='/search'>
+										Add a book
+									</Link>
+									
 								</div>
 							</div>
-						</div>
-					</div>
-
-					<div className="open-search">
-						<a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-					</div>
-				</div>
-				)}
+						)
+					}
+				/>
 			
 			</div>
 		)
